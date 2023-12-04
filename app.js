fd7f7372ac5cead75b2b388fc8ffd3034eda4e07
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-12-04 17:33:36 +0100

    validateForm func now displays correct error msg based on input

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const form = document.querySelector(".form");
 const message = document.querySelector(".message");
+const validationBox = document.querySelector(".validation-box");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    validateZip;
-})
-
-// const email = document.getElementById("email-field");
-// const validateEmail = function() {
-//     console.log(email.validity);
-// }
-
-// const country = document.getElementById("country-field");
-// const validateCountry = function() {
-//     console.log(country.validity);
-// }
-
+const email = document.getElementById("email-field");
+const country = document.getElementById("country-field");
 const zip = document.getElementById("zip-field");
-const validateZip = function() {
-    if (!zip.validity){
-        message.textContent = validationMessage;
-    }
-}
-
-
+const password = document.getElementById("password-field");
+const passwordConf = document.getElementById("password-conf-field");
 
 // Requirements:
 // 1) Valid Email (@ sign and domain included)
@@ -36,3 +19,52 @@ const validateZip = function() {
 // - 
 // Confirm Password:
 // - has to match password
+
+const validateForm = function (e) {
+    e.preventDefault();
+    const errors = [];
+
+    // Email
+    if (!email.checkValidity()){
+        errors.push("&bull; Email must be valid.")
+    }
+
+    // Country
+    if (!country.checkValidity()){
+        errors.push("&bull; Please select a valid country.")
+    }
+
+    // ZIP
+    if (!zip.checkValidity()){
+        errors.push("&bull; ZIP code must be valid.")
+    }
+
+    // Password
+    if (!password.checkValidity()){
+        errors.push(`&bull; Password must be: 
+        <br>- at least 5 characters
+        <br>- contain at least one number
+        <br>- contain at least one symbol`);
+    }
+
+    // Confirmation
+    if (password.value && passwordConf.value && passwordConf.value !== password.value) {
+        errors.push(`Passwords do not match.`);
+    }
+
+    // Error Message
+    if (errors.length > 0){
+        while (validationBox.firstChild){
+            validationBox.removeChild(validationBox.firstChild)
+        }
+
+        for (let i = 0; i < errors.length; i++){
+            const newError = document.createElement("p");
+            newError.classList.add("message");
+            newError.innerHTML = `${errors[i]}`;
+            validationBox.appendChild(newError);
+        }
+    }
+}
+
+form.addEventListener("change", validateForm);
